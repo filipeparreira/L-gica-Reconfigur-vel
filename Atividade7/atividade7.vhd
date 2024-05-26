dd56c95a6b248cf131d58b94a378c765ca014948
filipeparreira <filipeaugusto@alunos.utfpr.edu.br>
2024-05-26 17:22:13 -0300

    mod: atividade7.vhd 'Começando a fazer o processo de contar'

--- Atividade7/atividade7.vhd
+++ Atividade7/atividade7.vhd
@@ -1,16 +1,26 @@
 library ieee;
 use ieee.std_logic_1164;
 
-entity atividade7 is port(
-	reset, pause, mudar, up, down: in std_logic;
-	ssd1, ssd2, ssd3, ssd4: out std_logic_vector(6 DOWNTO 0)
-);
+entity atividade7 is 
+	generic(
+		f_clk: integer:= 50_000_000
+	);
+
+	port(
+		reset, pause, mudar, up, down, clk: in std_logic;
+		ssd1, ssd2, ssd3, ssd4: out std_logic_vector(6 DOWNTO 0)
+	);
 end atividade7;
 
 architecture main of atividade7 is 
-
+	
 begin 
-
-
+	process(clk, reset, pause) 
+		variable segundos: integer range 0 to 9999;
+		variable contador: integer range 0 to f_clk'high;
+	begin
+		if rising_edge(clk) then 
+			if contador < f_clk then 
+				contador := contador + 1;
 
 end main;
