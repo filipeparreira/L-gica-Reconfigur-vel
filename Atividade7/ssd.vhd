57916f203ee7609cc06f975279361504a3fb26b9
filipeparreira <filipeaugusto@alunos.utfpr.edu.br>
2024-05-29 17:48:38 -0300

    add: ssd.vhd 'Adicionado o arquivo e o corpo do código para o PROCEDURE'

---
+++ Atividade7/ssd.vhd
@@ -0,0 +1,15 @@
+library ieee;
+use ieee.std_logic_1164;
+
+package ssd is 
+	procedure ssd_numbers(in1, in2, in3, in4: in std_logic_vector(6 downto 0);
+									out1, out2, out3, out4: out std_logic_vector(6 downto 0));
+end ssd;
+
+package body ssd is
+	procedure ssd_numbers(in1, in2, in3, in4: in std_logic_vector(6 downto 0);
+									out1, out2, out3, out4: out std_logic_vector(6 downto 0)) is 
+	begin 
+	
+	end ssd_numbers;
+end ssd;
