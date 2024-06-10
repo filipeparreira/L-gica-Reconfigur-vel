029abc09bde41ed6cd08117cf6d64d4fd5f4764d
filipeparreira <filipeaugusto@alunos.utfpr.edu.br>
2024-06-10 17:01:44 -0300

    upt: Atividade7 'Salvando os arquivos alterados em sala'

--- Atividade7/atividade7.vhd
+++ Atividade7/atividade7.vhd
@@ -8,8 +8,8 @@ entity atividade7 is
 	);
 
 	port(
-		reset, pause, mudar, up, down: in bit;
-		clk: in std_logic;
+		reset, pause, up, down: in bit;
+		clk, mudar: in std_logic;
 		ssd1, ssd2, ssd3, ssd4: out std_logic_vector(6 DOWNTO 0)
 	);
 end atividade7;
@@ -19,33 +19,41 @@ architecture main of atividade7 is
 begin 
 	
 	process(clk, reset, mudar, pause, up, down) 
-		variable segundos, segundos_aux: integer range 0 to 9999 := 9999;
+		variable segundos, segundos_aux: integer range 0 to 9999 := 0;
 		variable contador: integer range 0 to f_clk'high;
 	begin
+		
 		ssd_numbers(segundos, ssd1, ssd2, ssd3, ssd4);
 		if reset = '0' then
-			segundos := segundos'high;
+			segundos := segundos_aux;
+		elsif mudar = '0' and rising_edge(clk) and pause = '1' then
+			if contador < f_clk/13 then 
+				contador := contador + 1;
+			else 
+				contador := 0;
+				if up = '1' and down ='0' then
+					segundos := segundos + 1;
+					if segundos > segundos'high then
+						segundos := segundos'high;
+					end if;
+				elsif up = '0' and down = '1' then
+					segundos := segundos - 1;
+					if segundos < 0 or segundos = 0 then
+						segundos := 0;
+					end if;
+				end if;
+				segundos_aux := segundos;
+			end if;
 		elsif rising_edge(clk) and pause = '0' then
 			if contador < f_clk then 
 				contador := contador + 1;
 			else 
-				contador := 0;
-				segundos := segundos - 1;				
-				if segundos < 0 or segundos = 0 then
-					segundos := 9999;
+				
+				if segundos = 0 then
+					contador := f_clk;
+				else
 					contador := 0;
-				end if;
-			end if;
-		elsif rising_edge(clk) and (up = '1' or down = '1') then
-			if up = '1' and down = '0' and mudar = '0' then
-				segundos := segundos + 10;
-				if segundos > segundos'high then
-					segundos := segundos'high;
-				end if;
-			elsif up = '0' and down = '1' and mudar = '0' then 
-				segundos := segundos - 10;
-				if segundos < 0 or segundos = 0 then 
-					segundos := 0;
+					segundos := segundos - 1;				
 				end if;
 			end if;
 		end if;
