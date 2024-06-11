1d9cb5731c6385ef3d7f892d64155da9e14c7b93
filipeparreira <filipeaugusto@alunos.utfpr.edu.br>
2024-06-10 21:30:10 -0300

    mod: atividade7.vhd e ssd.vhd 'Consertado o tempo aleatorio'

--- Atividade7/atividade7.vhd
+++ Atividade7/atividade7.vhd
@@ -19,11 +19,43 @@ architecture main of atividade7 is
 begin 
 	
 	process(clk, reset, mudar, pause, up, down) 
-		variable segundos, segundos_aux: integer range 0 to 9999 := 0;
+		variable segundos, segundos_aux: integer := 0;
 		variable contador: integer range 0 to f_clk'high;
 	begin
 		
-		ssd_numbers(segundos, ssd1, ssd2, ssd3, ssd4);
+		ssd_numbers_int(segundos, ssd1, ssd2, ssd3, ssd4);
+		
+		if  reset = '0' then
+			--segundos := segundos_aux
+		elsif rising_edge(clk) and mudar = '0' and pause = '1' then
+			if contador < f_clk/13 then 
+				contador := contador + 1;
+			else 
+				contador := 0;
+				if up = '1' and down ='0' then
+					segundos := segundos + 1;
+					if segundos > segundos'high then
+						segundos := segundos'high;
+					end if;
+				elsif up = '0' and down = '1' then
+					segundos := segundos - 1;
+					if segundos < 0 or segundos = 0 then
+						segundos := 0;
+					end if;
+				end if;
+				segundos_aux := segundos;
+			end if;
+		
+		elsif rising_edge(clk) and pause = '0' then
+			if contador = f_clk and segundos > 0 then 
+				segundos := segundos - 1;
+				contador := 0;
+			else 
+				contador := contador + 1;
+			end if;			
+		end if;
+		
+		/*
 		if reset = '0' then
 			segundos := segundos_aux;
 		elsif mudar = '0' and rising_edge(clk) and pause = '1' then
@@ -45,17 +77,14 @@ begin
 				segundos_aux := segundos;
 			end if;
 		elsif rising_edge(clk) and pause = '0' then
-			if contador < f_clk then 
-				contador := contador + 1;
+			if contador = f_clk and segundos > 0 then
+				segundos := segundos - 1;
+				contador := 0;
 			else 
-				
-				if segundos = 0 then
-					contador := f_clk;
-				else
-					contador := 0;
-					segundos := segundos - 1;				
-				end if;
-			end if;
+				contador := contador + 1;
+			end if;	
 		end if;
+		*/
+		
 	end process;
 end main;
--- Atividade7/ssd.vhd
+++ Atividade7/ssd.vhd
@@ -3,12 +3,14 @@ use ieee.std_logic_1164.all;
 use work.bcd.all;
 
 package ssd is 
-	procedure ssd_numbers(segundos: in integer range 0 to 9999;
+	procedure ssd_numbers(variable segundos: in integer;
+									signal out1, out2, out3, out4: out std_logic_vector(6 downto 0));
+	procedure ssd_numbers_int(segundos: in integer;
 									signal out1, out2, out3, out4: out std_logic_vector(6 downto 0));
 end ssd;
 
 package body ssd is
-	procedure ssd_numbers(variable segundos: in integer range 0 to 9999;
+	procedure ssd_numbers(variable segundos: in integer;
 									signal out1, out2, out3, out4: out std_logic_vector(6 downto 0)) is 
 		variable temp: integer range 0 to 9999;
 		variable milhar, centena, dezena, unidade: integer range 0 to 9;
@@ -31,5 +33,30 @@ package body ssd is
 	  conversor(dezena, out2);
 	  conversor(centena, out3);
 	  conversor(milhar, out4);
-	end ssd_numbers;
+	end procedure;
+	
+	procedure ssd_numbers_int(segundos: in integer;
+									signal out1, out2, out3, out4: out std_logic_vector(6 downto 0)) is 
+		variable temp: integer range 0 to 9999;
+		variable milhar, centena, dezena, unidade: integer range 0 to 9;
+   begin
+	  temp := segundos;
+	  
+	  -- Extraindo os dígitos das casas decimais
+	  milhar := temp / 1000;        -- Dígito dos milhares
+	  temp := temp mod 1000;
+	  
+	  centena := temp / 100;         -- Dígito das centenas
+	  temp := temp mod 100;
+	  
+	  dezena := temp / 10;          -- Dígito das dezenas
+	  temp := temp mod 10;
+	  
+	  unidade := temp;               -- Dígito das unidades
+	  
+	  conversor(unidade, out1);
+	  conversor(dezena, out2);
+	  conversor(centena, out3);
+	  conversor(milhar, out4);
+	end procedure;
 end ssd;
