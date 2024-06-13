c82321ce6c0d88c2e4e08c3c0a2dd1ac3e235905
filipeparreira <filipeaugusto@alunos.utfpr.edu.br>
2024-06-13 15:17:53 -0300

    mod: bcd.vhd 'Adicionando comentarios'

--- Atividade7/bcd.vhd
+++ Atividade7/bcd.vhd
@@ -1,47 +1,49 @@
+-- DECLARAÇÃO DE BIBLIOTECAS/PACOTE
 library ieee;
 use ieee.std_logic_1164.all;
 
+-- DECLARAÇÃO DO PACKAGE
 package bcd is
+	-- PROCEDURE conversor
 	procedure conversor(variable input: in integer range 0 to 9;
 								signal saida: out std_logic_vector(6 downto 0));
 end package;
 
+-- SEÇÃO DO CORPO DO PACOTE
 package body bcd is 
-	 procedure conversor(variable input: in integer range 0 to 9;
+	
+	-- Especificação da lógica do PROCEDURE, seus parametros são
+		-- uma variável como entrada, e um sinal como saída
+	procedure conversor(variable input: in integer range 0 to 9;
 								signal saida: out std_logic_vector(6 downto 0)) is 
 		begin 
+			
+			-- Analisa o valor da entrada, dependendo do valor 
+				-- atribui a saída um vetor de bits que corresponde
+				-- ao número inteiro contido na entrada.
 			case input is 
 				when 0 => 
 					saida <= "1000000";
 				when 1 =>
 					saida <= "1111001";
 				when 2 => 
-					--saida <= "1101101";
 					saida <= "0100100";
 				when 3 =>
-					--saida <= "1111001";
 					saida <= "0110000";
 				when 4 => 
-					--saida <= "0110011";
 					saida <= "0011001";
 				when 5 =>
-					--saida <= "1011011";
 					saida <= "0010010";
 				when 6 =>
-					--saida <= "1011111";
 					saida <= "0000010";
 				when 7 =>
-					--saida <= "1110000";
 					saida <= "1111000";
 				when 8 =>
-					--saida <= "1111111";
 					saida <= "0000000";
 				when 9 =>
-					--saida <= "1111011";
 					saida <= "0010000";
 				when others =>
-					--saida <= "1001111"; -- Valor de retorno padrão
-					saida <= "0110000";
+					saida <= "0110000"; -- Valor de retorno padrão
 			end case;
 		end procedure conversor;
 end package body bcd;
