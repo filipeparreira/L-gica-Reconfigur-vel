17e9166275cc5abecb0c138ed4a7717411787ef2
filipeparreira <filipeaugusto@alunos.utfpr.edu.br>
2024-06-14 09:38:55 -0300

    mod: ssd.vhd 'Comentado código'

--- Atividade7/ssd.vhd
+++ Atividade7/ssd.vhd
@@ -1,62 +1,49 @@
+-- DECLARAÇÃO DE BIBLIOTECAS/PACOTES
 library ieee;
 use ieee.std_logic_1164.all;
-use work.bcd.all;
+use work.bcd.all; -- USANDO ARQUIVO bcd.vhd
 
-package ssd is 
-	procedure ssd_numbers(variable segundos: in integer;
-									signal out1, out2, out3, out4: out std_logic_vector(6 downto 0));
+
+-- DECLARAÇÃO DO CONTEUDO DO PACKAGE
+package ssd is
+	-- DOIS PARAMETROS: 
+		-- SEGUNDOS: Variavel inteira
+		-- OUT1, OUT2, OUT3, OUT4: SINAL, cada display utilizado
 	procedure ssd_numbers_int(segundos: in integer;
 									signal out1, out2, out3, out4: out std_logic_vector(6 downto 0));
 end ssd;
 
+-- CORPO DO PACOTE
 package body ssd is
-	procedure ssd_numbers(variable segundos: in integer;
-									signal out1, out2, out3, out4: out std_logic_vector(6 downto 0)) is 
-		variable temp: integer range 0 to 9999;
-		variable milhar, centena, dezena, unidade: integer range 0 to 9;
-   begin
-	  temp := segundos;
-	  
-	  -- Extraindo os dígitos das casas decimais
-	  milhar := temp / 1000;        -- Dígito dos milhares
-	  temp := temp mod 1000;
-	  
-	  centena := temp / 100;         -- Dígito das centenas
-	  temp := temp mod 100;
-	  
-	  dezena := temp / 10;          -- Dígito das dezenas
-	  temp := temp mod 10;
-	  
-	  unidade := temp;               -- Dígito das unidades
-	  
-	  conversor(unidade, out1);
-	  conversor(dezena, out2);
-	  conversor(centena, out3);
-	  conversor(milhar, out4);
-	end procedure;
-	
+
+	-- ESPECIFICAÇÃO LÓGICA DO PROCEDURE.
 	procedure ssd_numbers_int(segundos: in integer;
 									signal out1, out2, out3, out4: out std_logic_vector(6 downto 0)) is 
+	-- SEÇÃO DECLARATIVA
 		variable temp: integer range 0 to 9999;
 		variable milhar, centena, dezena, unidade: integer range 0 to 9;
    begin
-	  temp := segundos;
-	  
-	  -- Extraindo os dígitos das casas decimais
-	  milhar := temp / 1000;        -- Dígito dos milhares
-	  temp := temp mod 1000;
-	  
-	  centena := temp / 100;         -- Dígito das centenas
-	  temp := temp mod 100;
-	  
-	  dezena := temp / 10;          -- Dígito das dezenas
-	  temp := temp mod 10;
-	  
-	  unidade := temp;               -- Dígito das unidades
-	  
-	  conversor(unidade, out1);
-	  conversor(dezena, out2);
-	  conversor(centena, out3);
-	  conversor(milhar, out4);
+		
+		-- Variavel temporaria para segundos
+		temp := segundos;
+
+		-- Extraindo os dígitos das casas decimais
+		milhar := temp / 1000;        -- Dígito dos milhares
+		temp := temp mod 1000;
+
+		centena := temp / 100;         -- Dígito das centenas
+		temp := temp mod 100;
+
+		dezena := temp / 10;          -- Dígito das dezenas
+		temp := temp mod 10;
+
+		unidade := temp;               -- Dígito das unidades
+		
+		-- Utilizando o procedure do código bcd para converter
+			-- as casas decimais obtidas
+		conversor(unidade, out1);
+		conversor(dezena, out2);
+		conversor(centena, out3);
+		conversor(milhar, out4);
 	end procedure;
 end ssd;
